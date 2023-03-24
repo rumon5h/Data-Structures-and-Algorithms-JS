a14f6c14ea7faf6b152b49edac2eb2db57b90bd0
rumon5h <mdrumonkhan48519@gmail.com>
2023-03-24 16:25:07 +0600

    fibonacci sequence problem description

---
+++ fibonacci-sequence.js
@@ -0,0 +1,13 @@
+// Fibonacci sequence
+
+// Problem - Give a number (n) find the first (n) elements of the fibonacci sequence.
+
+// In mathematics, the Fibonacci sequence is a sequence in which each number is the sum of the tow preceding ones.
+
+// The first tow numbers in the sequence are 0 and 1.
+
+// fibonacci(2) = [0,1]
+// fibonacci(3) = [0,1,1]
+// fibonacci(7) = [0,1,1,2,3,5,8]
+
+
