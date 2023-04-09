149581e3a90c1a5e99a997a50f4ac52d4fa52716
rumon5h <mdrumonkhan48519@gmail.com>
2023-04-09 16:21:31 +0600

    factorial-of-a-number

--- fibonacci-sequence.js
+++ fibonacci-sequence.js
@@ -15,7 +15,7 @@ function fibonacci(n){
     const fib = [0,1];
 
     for (let i = 2; i < n; i++) {
-        fib[i] = fib[i -1] + fib[i -2]
+        fib[i] = fib[i - 1] + fib[i - 2]
     }
 
     return fib;
